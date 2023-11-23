9dbeb45127e9f7e20bc829c29c991d6022ed07b4
RITIK RANJAN <142659994+RitikShroff-11@users.noreply.github.com>
2023-11-23 17:55:54 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("form PredictionModel.html")
+    return render_template("index.html")
 
 @app.route("/predict", methods=["GET"])
 def predict():
